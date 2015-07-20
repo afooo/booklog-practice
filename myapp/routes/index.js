1f302681db59d0cd264a1a1f3aa2f7b156a17dad
Afoo Chang <foofoo.chang@gmail.com>
2015-07-20 23:37:15 +0800

    add basic routing

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// respond with "Hello World!" on the homepage
+router.get('/:user', function (req, res) {
+  res.render('index', { title: 'Hello ' + req.params.user + '!' });
+});
+
+// accept POST request on the homepage
+router.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+router.put('/', function (req, res) {
+  res.send('Got a PUT request');
+});
+
+// accept DELETE request at /user
+router.delete('/', function (req, res) {
+  res.send('Got a DELETE request');
+});
+
 module.exports = router;
